cb26fcaf349cd00340a0b9ab6f52f7b8fb89e94f
Jonathan Lin <johnlin0214@gmail.com>
2015-03-27 14:10:25 -0400

    Add test for save and getall of brand class

--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -55,6 +55,33 @@
             $this->assertEquals("Saucony", $result);
         }
 
+        function test_save()
+        {
+            $new_name = "Nike";
+            $new_brand = new Brand($new_name);
+            $new_brand->save();
+
+            $result = Brand::getAll();
+
+            $this->assertEquals([$new_brand], $result);
+
+        }
+
+        function test_getAll()
+        {
+            $new_name = "Puma";
+            $new_brand = new Brand($new_name);
+            $new_brand->save();
+
+            $new_name1 = "Nike";
+            $new_brand1 = new Brand($new_name1);
+            $new_brand1->save();
+
+            $result = Brand::getAll();
+
+            $this->assertEquals([$new_brand, $new_brand1], $result);
+        }
+
     }
 
 
