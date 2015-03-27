c616ce982b876c59176d69d636b16f38c9993d2a
Jonathan Lin <johnlin0214@gmail.com>
2015-03-27 16:43:32 -0400

    Add delete all functionality on stores.html.twig page

---
+++ app/app.php
@@ -0,0 +1,45 @@
+<?php
+        require_once __DIR__."/../vendor/autoload.php";
+        require_once __DIR__."/../src/Store.php";
+        require_once __DIR__."/../src/Brand.php";
+
+        $DB = new PDO('pgsql:host=localhost;dbname=shoes');
+
+        $app = new Silex\Application();
+        $app ['debug'] = true;
+
+        $app->register(new Silex\Provider\TwigServiceProvider, array(
+            'twig.path'=>__DIR__."/../views"
+        ));
+
+        use Symfony\Component\HttpFoundation\Request;
+        Request::enableHttpMethodParameterOverride();
+
+        $app->get('/', function() use($app) {
+            return $app['twig']->render("Home.html.twig");
+        });
+
+        $app->get('/stores', function () use ($app) {
+            return $app['twig']->render("stores.html.twig", array ('stores' => Store::getAll()));
+        });
+
+        $app->post('/stores', function() use ($app) {
+            $new_store = new Store($_POST['new_store']);
+            $new_store->save();
+            return $app['twig']->render("stores.html.twig", array ('stores' => Store::getAll()));
+        });
+
+        $app->post('/stores/delete', function() use ($app) {
+            Store::deleteAll();
+            return $app['twig']->render("stores.html.twig", array ('stores' => Store::getAll()));
+        });
+
+
+
+
+
+        return $app;
+
+
+
+ ?>
